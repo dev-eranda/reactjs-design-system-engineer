873f34cf019bdc04b4311420efa44abd8e10d9f9
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-05-27 11:01:02 +0530

    docs: doc changes

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Select } from '@ds.e/react'
+import { Select } from '@test.ds.e/react'
 
-import '@ds.e/scss/lib/Utilities.css'
-import '@ds.e/scss/lib/Text.css'
-import '@ds.e/scss/lib/Margin.css'
-import '@ds.e/scss/lib/Select.css'
-import '@ds.e/scss/lib/global.css'
+import '@test.ds.e/scss/lib/Utilities.css'
+import '@test.ds.e/scss/lib/Text.css'
+import '@test.ds.e/scss/lib/Margin.css'
+import '@test.ds.e/scss/lib/Select.css'
+import '@test.ds.e/scss/lib/global.css'
 
 const options = [{
     label: 'Strict Black',
