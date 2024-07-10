9f485e4c4d2963975d90138d25cd9268c3d8c3db
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-07-10 12:52:35 +0530

    fix: fixed storybook

--- packages/react/src/atoms/Color/Color.stories.tsx
+++ packages/react/src/atoms/Color/Color.stories.tsx
@@ -1,19 +1,52 @@
 import React from 'react'
-import Color from './Color'
-import { text, select } from '@storybook/addon-knobs'
+import Color, { ColorProps } from './Color'
 
 import { Spacing } from '@test.ds.e/foundation'
 
 // css
-import '@ds.e/scss/lib/Utilities.css'
+import '@test.ds.e/scss/lib/Utilities.css'
 
 export default {
     title: 'Atoms|Color'
 }
 
-export const Common = () => <Color hexCode={text('HexCode', 'pink')} />
+export const Common = (args: React.JSX.IntrinsicAttributes & ColorProps & { children?: React.ReactNode }) => (
+    <Color {...args} />
+)
+Common.args = {
+    primary: true,
+    hexCode: 'pink'
+}
 
-export const CustomDimensions = () => <Color
-hexCode={text('HexCode', 'pink')}
-width={select('Width', Object.values(Spacing), 'xxl')}
-height={select('Height', Object.values(Spacing), 'xxl')}  />
+export const CustomDimensions = (args: React.JSX.IntrinsicAttributes & ColorProps & { children?: React.ReactNode }) => (
+    <Color {...args} />
+)
+  
+CustomDimensions.args = {
+    primary: true,
+    hexCode: 'pink',
+    width: 'xxl',
+    height: 'xxl',
+    }
+
+CustomDimensions.argTypes = {
+    width: {
+        control: 'select',
+        options: Object.values(Spacing),
+    },
+    height: {
+        control: 'select',
+        options: Object.values(Spacing),
+        },
+    hexCode: {
+        control: 'color',
+    }
+}
+
+
+// export const Common = () => <Color hexCode={text('HexCode', 'pink')} />
+
+// export const CustomDimensions = () => <Color
+// hexCode={text('HexCode', 'pink')}
+// width={select('Width', Object.values(Spacing), 'xxl')}
+// height={select('Height', Object.values(Spacing), 'xxl')}  />
